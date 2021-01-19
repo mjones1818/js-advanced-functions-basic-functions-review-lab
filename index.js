fe86e43a9c82dbb7f868c0c41b38c654fa7e59be
mjones1818 <michael.jones0532@gmail.com>
2021-01-18 18:15:12 -0600

    commit tree 25f27f403d4c3509d91fb71d8b161947745fb393

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function saturdayFun(activity='roller-skate'){
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity='go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style="*"){
+  return function(adjective= 'special') {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
+let Calculator = {
+  add: function(num1,num2) {return num1+num2},
+  subtract: (num1,num2) => {return num1 - num2},
+  multiply: (num1,num2) => {return num1 * num2},
+  divide: (num1,num2) => {return num1 / num2}
+}
+
+function actionApplyer(startingNumber, arrayOfTransforms) {
+  if (arrayOfTransforms.length === 0) {
+    return startingNumber
+  }
+  let num = startingNumber
+  for (let i=0; i < arrayOfTransforms.length; i++){
+    num = arrayOfTransforms[i](num)
+  }
+  return num
+}
